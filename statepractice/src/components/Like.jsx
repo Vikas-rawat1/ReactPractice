3d9a7d0908afd68e8147605339c9fcab2795be32
vikas <rawatvikas689@gmail.com>
2024-09-05 00:19:54 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Like() {
   return (
-    <div>     <RiHeartFill
-    size={36} // set custom `width` and `height`
-    color="red" // set `fill` color
-    className="my-icon" // add custom class name
-/></div>
-  )
+    <div>
+      {" "}
+      <RiHeartFill
+        size={36} // set custom `width` and `height`
+        color="red" // set `fill` color
+        className="my-icon" // add custom class name
+      />
+    </div>
+  );
 }
 
-export default Like
+export default Like;
