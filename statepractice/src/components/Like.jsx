fcc52fece029e5b21302783360b9a6b9ed01fa41
vikas <rawatvikas689@gmail.com>
2024-09-05 00:28:27 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -2,7 +2,7 @@ import { RiHeartFill } from "@remixicon/react";
 import { useState } from "react";
 
 function Like() {
-  const [isLiked, setIsLiked] = useState(true);
+  const [isLiked, setIsLiked] = useState(false);
 
   const handleLikedChange = () => {
     setIsLiked(!isLiked);
