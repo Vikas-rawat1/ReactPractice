7d85e1c08c96e1051c98289bb6aa6b15d8b49496
vikas <rawatvikas689@gmail.com>
2024-09-05 00:20:22 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Like() {
   return (
     <div>
-      {" "}
+      
       <RiHeartFill
         size={36} // set custom `width` and `height`
         color="red" // set `fill` color
