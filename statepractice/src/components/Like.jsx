13dce02ea19868d072b8de3133291ae1c97d82e4
vikas <rawatvikas689@gmail.com>
2024-09-05 00:20:12 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -1,10 +1,11 @@
+import { RiHeartFill } from "@remixicon/react";
 import React from "react";
 
 function Like() {
   return (
     <div>
       {" "}
-      <RiHeartFil
+      <RiHeartFill
         size={36} // set custom `width` and `height`
         color="red" // set `fill` color
         className="my-icon" // add custom class name
