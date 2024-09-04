392114f1c77600827575941a26a324f31a6bbda8
vikas <rawatvikas689@gmail.com>
2024-09-05 00:21:34 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -7,7 +7,7 @@ function Like() {
       <RiHeartFill
         size={36} 
         color="red" 
-        className="my-icon" // add custom class name
+        className="my-icon"
       />
     </>
   );
