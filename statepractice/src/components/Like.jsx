b24cd26a57bee05b8ac6adaa054f3a85c30a7152
vikas <rawatvikas689@gmail.com>
2024-09-05 00:47:10 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -3,16 +3,22 @@ import { useState } from "react";
 
 function Like() {
   const [isLiked, setIsLiked] = useState(false);
+  // const [clicks, setClicks] = useState(0);
 
   const handleLikedChange = () => {
     setIsLiked(!isLiked);
-    
+    // setClicks(clicks + 1);
   };
   return (
     <>
-      <p>{isLiked}</p>
-      <RiHeartFill size={36} color="white" className="my-icon" />
-      <RiHeartFill size={36} color="red" className="my-icon" />
+      <p>Clicks= {clicks}</p>
+      <p onClick={handleLikedChange}>
+        {isLiked ? (
+          <RiHeartFill size={36} color="red" className="my-icon" />
+        ) : (
+          <RiHeartFill size={36} color="white" className="my-icon" />
+        )}
+      </p>
     </>
   );
 }
