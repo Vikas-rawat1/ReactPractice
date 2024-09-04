afcbf98de2b363a6075e65fa4b9bb85857336f65
vikas <rawatvikas689@gmail.com>
2024-09-05 00:24:55 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -1,6 +1,8 @@
 import { RiHeartFill } from "@remixicon/react";
+import { useState } from "react";
 
 function Like() {
+  useState()
   return (
     <>
       <RiHeartFill size={36} color="red" className="my-icon" />
