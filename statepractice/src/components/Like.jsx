edc9c04834a02dc395f94ce920247475124e30a7
vikas <rawatvikas689@gmail.com>
2024-09-05 00:20:29 +0530

    commit

--- statepractice/src/components/Like.jsx
+++ statepractice/src/components/Like.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function Like() {
   return (
-    <div>
+    <>
       
       <RiHeartFill
         size={36} // set custom `width` and `height`
         color="red" // set `fill` color
         className="my-icon" // add custom class name
       />
-    </div>
+    </>
   );
 }
 
