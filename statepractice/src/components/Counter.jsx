e39945bafdbefdc32e3c092f6a3e210359ad8079
vikas <rawatvikas689@gmail.com>
2024-09-04 23:25:00 +0530

    commit

--- statepractice/src/components/Counter.jsx
+++ statepractice/src/components/Counter.jsx
@@ -6,7 +6,7 @@ function Counter() {
   // const handleClickOnCount = () => {
   //   setCount(count + 1);
   // };
-  // console.log(count);
+  // console.log(cou
   return (
     <div>
       {/* <button onClick={handleClickOnCount}>Click me {count}</button> */}
