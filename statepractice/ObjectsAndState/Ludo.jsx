6050803f78c083a0cd7d5559b1d229cab2b41ad5
vikas <rawatvikas689@gmail.com>
2024-09-09 21:36:15 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Ludo() {
   //instead of creating a sperate STATE we will create a object
-  const [moves, setMoves] = useState({blue:0,green:0,yellow:0});
+  const [moves, setMoves] = useState({ blue: 0, green: 0, yellow: 0, red: 0 });
 
   return (
     <>
