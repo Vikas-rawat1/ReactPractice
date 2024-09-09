12d440c9ce8723bb3aafb61705475d3f4b99e20f
vikas <rawatvikas689@gmail.com>
2024-09-09 21:17:43 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -3,6 +3,12 @@ function Ludo() {
     <div>
       <p>Blue Move</p>
       <button>+1</button>
+      <p>Blue Move</p>
+      <button>+1</button>
+      <p>Blue Move</p>
+      <button>+1</button>
+      <p>Blue Move</p>
+      <button>+1</button>
     </div>
   </>;}
 export default Ludo;
