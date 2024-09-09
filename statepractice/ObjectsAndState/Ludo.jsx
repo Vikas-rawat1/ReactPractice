d7baf442ff6d1c8f98c8b704bd8e364149e6e03e
vikas <rawatvikas689@gmail.com>
2024-09-09 22:02:39 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -5,25 +5,33 @@ function Ludo() {
   const [moves, setMoves] = useState({ blue: 0, green: 0, yellow: 0, red: 0 });
 
   let updateBlue = () => {
-    moves.blue += 1;
-    moves.green += 1;
-    console.log(moves)
-    setMoves({...moves})// if we want to change the object state ,then we need to copy the state through spread operator and it will render it onclick 
+    // moves.blue += 1;
+    // console.log(moves);
+    setMoves({ ...moves, blue: (moves.blue += 1) }); // if we want to change the object state ,then we need to copy the state through spread operator and it will render it onclick
   };
 
   return (
     <>
       <div>
         <p>Blue Move ={moves.blue}</p>
-        <button onClick={updateBlue} style={{ backgroundColor: "blue" }}>+1</button>
+        <button onClick={updateBlue} style={{ backgroundColor: "blue" }}>
+          +1
+        </button>
         <p>Yellow Move ={moves.yellow}</p>
-        <button style={{ backgroundColor: "yellow", color: "black" }}>
+        <button
+          onClick={updateBlue}
+          style={{ backgroundColor: "yellow", color: "black" }}
+        >
           +1
         </button>
         <p>Green Move ={moves.green}</p>
-        <button style={{ backgroundColor: "green" }}>+1</button>
+        <button onClick={updateBlue} style={{ backgroundColor: "green" }}>
+          +1
+        </button>
         <p>Red Move ={moves.red}</p>
-        <button style={{ backgroundColor: "red" }}>+1</button>
+        <button onClick={updateBlue} style={{ backgroundColor: "red" }}>
+          +1
+        </button>
       </div>
     </>
   );
