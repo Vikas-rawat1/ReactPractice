7076a27d064c25e5eec3da8ebcd84ba67bfd9eb6
vikas <rawatvikas689@gmail.com>
2024-09-09 21:44:21 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -4,13 +4,20 @@ function Ludo() {
   //instead of creating a sperate STATE we will create a object
   const [moves, setMoves] = useState({ blue: 0, green: 0, yellow: 0, red: 0 });
 
+  let updateBlue = () => {
+    moves.blue += 1;
+    console.log(moves)
+  };
+
   return (
     <>
       <div>
         <p>Blue Move ={moves.blue}</p>
-        <button style={{ backgroundColor: "blue" }}>+1</button>
+        <button onClick={updateBlue} style={{ backgroundColor: "blue" }}>+1</button>
         <p>Yellow Move ={moves.yellow}</p>
-        <button style={{ backgroundColor: "yellow" ,color:"black"}}>+1</button>
+        <button style={{ backgroundColor: "yellow", color: "black" }}>
+          +1
+        </button>
         <p>Green Move ={moves.green}</p>
         <button style={{ backgroundColor: "green" }}>+1</button>
         <p>Red Move ={moves.red}</p>
