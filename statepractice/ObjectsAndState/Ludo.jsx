c4491e11d11815464bbb400d9026930c43e4ccc3
vikas <rawatvikas689@gmail.com>
2024-09-09 21:39:59 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -8,13 +8,13 @@ function Ludo() {
     <>
       <div>
         <p>Blue Move ={moves.blue}</p>
-        <button style={{backgroundColor:"blue"}}>+1</button>
+        <button style={{ backgroundColor: "blue" }}>+1</button>
         <p>Yellow Move ={moves.yellow}</p>
-        <button style={{backgroundColor:"yellow"}}>+1</button>
+        <button style={{ backgroundColor: "yellow" }}>+1</button>
         <p>Green Move ={moves.green}</p>
-        <button style={{backgroundColor:"green"}}>+1</button>
+        <button style={{ backgroundColor: "green" }}>+1</button>
         <p>Red Move ={moves.red}</p>
-        <button style={{backgroundColor:"red"}}>+1</button>
+        <button style={{ backgroundColor: "red" }}>+1</button>
       </div>
     </>
   );
