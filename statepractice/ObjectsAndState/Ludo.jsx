5354a367ce99b828b73aeaeb063a9ee94025ca4e
vikas <rawatvikas689@gmail.com>
2024-09-10 21:55:28 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -19,7 +19,7 @@ function Ludo() {
       return { ...prevMoves, yellow: prevMoves.yellow + 1 };
     });
   };   
-  let updateGreen = () => {
+  let updateGr een = () => {
     // moves.green += 1;
     // console.log(moves);
     setMoves({ ...moves, green: (moves.green += 1) });
