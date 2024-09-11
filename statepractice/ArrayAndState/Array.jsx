2ddf04e77385c2a844a90a297659a46c82c66456
vikas <rawatvikas689@gmail.com>
2024-09-11 09:41:10 +0530

    commit

--- statepractice/ArrayAndState/Array.jsx
+++ statepractice/ArrayAndState/Array.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 
 function Array() {
   const [move, setMove] = useState({ blue: 0 });
+  let updateBlue = () => {
+    // move.blue += 1;
+    setMove({ ...move, blue: (move.blue += 1) });
+    // console.log(move);
+  };
+
   let [arr, setArr] = useState(["moves pass"]); //passing / initializing with array
   return (
     <>
       <div>
         <p>Array and State Practice</p>
         <p>Moves= {move.blue}</p>
-        <button style={{ backgroundColor: "blue" }}>Move</button>
+        <button onClick={updateBlue} style={{ backgroundColor: "blue" }}>
+          Move
+        </button>
       </div>
     </>
   );
