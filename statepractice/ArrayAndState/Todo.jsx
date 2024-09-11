7232a451a844837900d0e175481d6d9e7eb3f067
vikas <rawatvikas689@gmail.com>
2024-09-11 10:44:23 +0530

    commit

--- statepractice/ArrayAndState/Todo.jsx
+++ statepractice/ArrayAndState/Todo.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from "react";
 
 function Todo() {
-  let [inputVal, setInputVal] = useState();
+  let [inputVal, setInputVal] = useState("");
+  let [todo, setTodo] = useState([]);
+
+  console.log(inputVal);
+  // setInputVal(inputVal);
+
+  let addNewTask = () => {
+    console.log("New task added");
+    setTodo([...todo, inputVal]);
+  };
+
   return (
     <>
       <div>
         <input
-          style={{ outline: "none" }}
+          style={{ outline: "none", height: "4vw" }}
           type="text"
           placeholder="Add a Task"
+          value={inputVal}
+          onChange={(e) => {
+            setInputVal(e.target.value);
+          }}
         />
+        <button
+          onClick={addNewTask}
+          style={{ backgroundColor: "blue", outline: "none" }}
+        >
+          Add
+        </button>
+        <div>
+          <ul>
+            {todo.map((val, inx) => {
+              return <li key={inx}>{val}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
