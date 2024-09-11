d95f986f7e9608f6ce15bc6c426ea78ddeceea9e
vikas <rawatvikas689@gmail.com>
2024-09-11 11:09:39 +0530

    commit

--- statepractice/ArrayAndState/Todo.jsx
+++ statepractice/ArrayAndState/Todo.jsx
@@ -1,3 +1,4 @@
+import { RiDeleteBin2Fill } from "@remixicon/react";
 import React, { useState } from "react";
 
 function Todo() {
@@ -8,16 +9,25 @@ function Todo() {
   // setInputVal(inputVal);
 
   let addNewTask = () => {
-    console.log("New task added");
+    // console.log("New task added");
+  if(inputVal.trim()===""){
+    alert("Please enter a task name")
+    return;
+  }
+
     setTodo([...todo, inputVal]);
-    setInputVal(""); //clear the input field after adding a new task
+    setInputVal(""); //clear the input field after adding a new task\
+    
   };
 
   return (
     <>
       <div>
         <input
-          style={{ outline: "none", height: "4vw" }}
+          style={{
+            outline: "none",
+            height: "3vw",
+          }}
           type="text"
           placeholder="Add a Task"
           value={inputVal}
@@ -27,7 +37,11 @@ function Todo() {
         />
         <button
           onClick={addNewTask}
-          style={{ backgroundColor: "blue", outline: "none" }}
+          style={{
+            backgroundColor: "blue",
+            outline: "none",
+            marginLeft: "2vw",
+          }}
         >
           Add
         </button>
@@ -36,25 +50,27 @@ function Todo() {
             {todo.map((val, inx) => {
               return (
                 <div key={inx}>
-                  {/* <li>
-                <button onClick={() => {
-                  setTodo(todo.filter((item, index) => index!== inx));
-                }}>
-                  Delete
-                </button>
-                {val}
-              </li> */}
                   <div
                     style={{
                       display: "flex",
-                      height: "10vw",
+                      height: "4vw",
                       justifyContent: "center",
                       alignItems: "center",
                       gap: "2vw",
                     }}
                   >
                     <li style={{ listStyle: "none" }}>{val}</li>
-                    <button>Delete</button>
+                    <button
+                      // todo.splice(index, 1) // this will also remove the item from the array but not from the UI
+                      // instead of this, we use filter method which returns a new array without the item at the specified index
+                      // and does not mutate the original array.
+                      onClick={() => {
+                        setTodo(todo.filter((val, index) => index !== inx));
+                      }}
+                      style={{ backgroundColor: "transparent" }}
+                    >
+                      <RiDeleteBin2Fill />
+                    </button>
                   </div>
                 </div>
               );
