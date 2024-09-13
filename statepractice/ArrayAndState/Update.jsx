eb92b15d283cd0bc0c8da38adef928874b587d5c
vikas <rawatvikas689@gmail.com>
2024-09-13 21:31:38 +0530

    commit

--- statepractice/ArrayAndState/Update.jsx
+++ statepractice/ArrayAndState/Update.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
 function Update() {
-  const [inputVal, setInputVal] = useState();
+  const [inputVal, setInputVal] = useState("");
+  console.log(input)
   return (
     <>
       {/* Update The array */}
-      <div></div>
+      <div>
+        <input type="text" placeholder="Add a task" />
+        <button>ADD</button>
+      </div>
     </>
   );
 }
