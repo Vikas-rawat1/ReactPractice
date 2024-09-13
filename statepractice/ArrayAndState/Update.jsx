ad34c939cc6586b6a68af04fb35e568a04b22076
vikas <rawatvikas689@gmail.com>
2024-09-13 21:34:53 +0530

    commit

--- statepractice/ArrayAndState/Update.jsx
+++ statepractice/ArrayAndState/Update.jsx
@@ -2,12 +2,18 @@ import React, { useState } from "react";
 
 function Update() {
   const [inputVal, setInputVal] = useState("");
+  const [] = useState();
   // console.log(inputVal)
   return (
     <>
       {/* Update The array */}
       <div>
-        <input type="text" placeholder="Add a task" value={inputVal} onChange={(e)=>setInputVal(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Add a task"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
         <button>ADD</button>
       </div>
     </>
