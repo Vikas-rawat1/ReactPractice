901f985fd93c3e1f17bbcf143dbfdd445dbcede9
vikas <rawatvikas689@gmail.com>
2024-09-14 21:13:10 +0530

    commit

--- statepractice/ArrayAndState/UpdateOneArra.jsx
+++ statepractice/ArrayAndState/UpdateOneArra.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function UpdateOneArra() {
+  const [inputVal, setInputVal] = useState("");
+  const [task, setTask] = useState([]);
+
+  let handleClick = (val) => {
+    setTask([...task, (val = inputVal)]);
+    setInputVal("");
+  };
+
   return (
     <></>
   )
