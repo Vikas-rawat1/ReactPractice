160650ee34cf7429140f4358cd81a652a1209840
vikas <rawatvikas689@gmail.com>
2024-09-14 21:13:34 +0530

    commit

--- statepractice/ArrayAndState/UpdateOneArra.jsx
+++ statepractice/ArrayAndState/UpdateOneArra.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function UpdateOneArra() {
   const [inputVal, setInputVal] = useState("");
@@ -10,7 +10,33 @@ function UpdateOneArra() {
   };
 
   return (
-    <></>
+    <> <div>
+    <input
+      type="text"
+      placeholder="Add a task"
+      value={inputVal}
+      onChange={(e) => setInputVal(e.target.value)}
+    />
+    <button onClick={handleClick}>ADD</button>
+    <div>
+      {task.map((val, inx) => (
+        <div key={inx}>
+          {val}
+          <button
+            onClick={() => {
+              return setTask(task.filter((val, index) => index !== inx));
+            }}
+          >
+            <RiDeleteBinFill />
+          </button>
+        </div>
+      ))}
+    </div>
+
+    {/* Update all todos  */}
+    <button onClick={updateTaskUpper}>Update Task Upper</button>
+    <button onClick={updateTaskLower}>Update Task Lower</button>
+  </div></>
   )
 }
 
