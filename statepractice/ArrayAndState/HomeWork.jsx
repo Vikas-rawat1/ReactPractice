0465f58b7eb6cadd83e01c45a1ed66f6c58e7565
vikas <rawatvikas689@gmail.com>
2024-09-17 19:59:44 +0530

    commit

--- statepractice/ArrayAndState/HomeWork.jsx
+++ statepractice/ArrayAndState/HomeWork.jsx
@@ -2,16 +2,57 @@ import React, { useState } from "react";
 
 function HomeWork() {
   const [random, setRandom] = useState();
+  const [isSum, setIsSum] = useState();
   let genrateRandomNumber = () => {
-    setRandom(Math.random() * 100);
-    console.log(random)
+    const randomTicket = Math.floor(100 + Math.random() * 900);
+    setRandom(randomTicket.toString());
+    // console.log(random);
+
+    const sumOfDigit = randomTicket
+      .toString()
+      .split("")
+      .reduce((sum, digit) => sum + parseInt(digit), 0);
+    // console.log(sumOfDigit);
+
+    if (sumOfDigit === 12) {
+      setIsSum(true);
+    } else {
+      setIsSum(false);
+    }
   };
 
   return (
     <div>
-      <p>Lottery</p>
-      <p>Lottery Ticket = </p>
-      <button onClick={genrateRandomNumber}>Generate Ticket</button>
+      <p>Lottery Game 🎟️</p>
+      <p>Lottery Ticket ={random !== null ? random : "No Ticket Found"} </p>
+      <button
+        onClick={genrateRandomNumber}
+        style={{
+          padding: "10px 20px",
+          backgroundColor: "#4CAF50",
+          color: "white",
+          fontSize: "16px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+      >
+        Generate Ticket
+      </button>
+      {random && (
+        <>
+          {/* <p>
+          {isSum === true
+            ? "🎉 The sum of the digits is 15! You win!"
+            : "❌ The sum of the digits is not 15. Try again!"}
+        </p> */}
+          <p>
+            {isSum === true
+              ? "🎉You win! Congratulations"
+              : "❌ Try again , Better Luck Next Time!"}
+          </p>
+        </>
+      )}
     </div>
   );
 }
