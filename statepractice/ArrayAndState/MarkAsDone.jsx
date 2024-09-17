deecba0a4d591351dabc4dc19214dcb9f9e507b9
vikas <rawatvikas689@gmail.com>
2024-09-17 10:35:02 +0530

    commit

--- statepractice/ArrayAndState/MarkAsDone.jsx
+++ statepractice/ArrayAndState/MarkAsDone.jsx
@@ -2,10 +2,29 @@ import React, { useState } from "react";
 
 function MarkAsDone() {
   const [inputVal, setInputVal] = useState("");
-  console.log(inputVal);
+  // console.log(inputVal);
+  const [task, setTask] = useState([]);
+
+
+  let handleAddClick=()=>{
+    setTask()
+  }
+
   return (
     <>
-      <input type="text" placeholder="Add task" value={inputVal} onChange={(e)=>console.log(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Add task"
+        value={inputVal}
+        onChange={(e) => setInputVal(e.target.value)}
+      />
+      <button onClick={}>Add</button>
+
+      <div>
+        {task.map((task, index) => (
+          <p>{task}</p>
+        ))}
+      </div>
     </>
   );
 }
