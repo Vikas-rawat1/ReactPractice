88dbb79448a016e5579c76f2b83855a31b304b96
vikas <rawatvikas689@gmail.com>
2024-09-17 12:28:51 +0530

    commit

--- statepractice/ArrayAndState/MarkAsDone.jsx
+++ statepractice/ArrayAndState/MarkAsDone.jsx
@@ -8,7 +8,7 @@ function MarkAsDone() {
   let handleAddClick = () => {
     setTask([...task, { val: inputVal, isDone: false }]);
     setInputVal("");
-    // console.l og(task)
+    // console.log(task)
   };
  
   let UpdateAllUpperCase = () => {
