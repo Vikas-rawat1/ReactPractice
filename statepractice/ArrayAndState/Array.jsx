d6ad9b9022129de095d34e597256a6ef34839b5e
vikas <rawatvikas689@gmail.com>
2024-09-20 16:03:28 +0530

    commit

--- statepractice/ArrayAndState/Array.jsx
+++ statepractice/ArrayAndState/Array.jsx
@@ -8,7 +8,7 @@ function Array() {
     // setMove({ ...move, blue: (move.blue += 1) });
     // console.log(move);
 
-    //CALLBACK 🌟🌟
+    CALLBACK 🌟🌟
     setMove((prev) => ({ ...prev, blue: (move.blue += 1) }));
 
     //yaha pe push ke wjhe se possible nahi he ye setmove ke wjhe se possible ho pa raha hai kuki yha pe ye original array me changes hora hai isliye wo re-render nahi kar paa raha hai
