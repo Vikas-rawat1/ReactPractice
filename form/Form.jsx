bfc7e06466404b34a44da626f7d180caee21c033
vikas <rawatvikas689@gmail.com>
2024-09-20 21:07:27 +0530

    commit

--- form/Form.jsx
+++ form/Form.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 function Form() {
-  return <>Form</>;
+  const [fullName, setFullName] = useState("");
+
+  let handleFullName = (e) => {
+  //  console.log((e.target))
+  setFullName(e.target.value)
+  };
+  return (
+    <>
+      <input type="text" placeholder="Enter text" value={fullName} onChange={handleFullName}/>
+      <button>Submit</button>
+    </>
+  );
 }
 
 export default Form;
