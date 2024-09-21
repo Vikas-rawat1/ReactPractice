9281bfc90674c018c5b332340ac02a1e7cb17eb1
vikas <rawatvikas689@gmail.com>
2024-09-21 19:23:49 +0530

    commit

--- form/HandlingMultipleInputs.jsx
+++ form/HandlingMultipleInputs.jsx
@@ -15,7 +15,7 @@ function HandlingMultipleInputs() {
   let handleInputChange = (e) => {
     let fieldname = e.target.name; //name is attribute
     let newVal = e.target.value; //vlass is attribute
-    console.log(fieldname);//fullName,,, obj ke andar ke fieldname
+    // console.log(fieldname);//fullName,,, obj ke andar ke fieldname
     // console.log(newVal); //fullName
 
     setFormData((currData) => {
