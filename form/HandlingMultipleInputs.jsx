dcba31b43fa32928e633ed4e67155f1b13d43f4e
vikas <rawatvikas689@gmail.com>
2024-09-21 14:02:02 +0530

    commit

--- form/HandlingMultipleInputs.jsx
+++ form/HandlingMultipleInputs.jsx
@@ -21,7 +21,7 @@ function HandlingMultipleInputs() {
     setFormData((currData) => {
       // 🌟🌟NORMAL WAY
 
-      currData[fieldname] = newVal; //[] ka meaning hai ki COMPUTED PROPERY NAME  iska matlab hai aap ek propery ko access karne ki kosis karna chate hai but uska excate name apkko nahi pta hai, apke paas ek variable hai jisme us propery ka name hai
+      // currData[fieldname] = newVal; //[] ka meaning hai ki COMPUTED PROPERY NAME  iska matlab hai aap ek propery ko access karne ki kosis karna chate hai but uska excate name apkko nahi pta hai, apke paas ek variable hai jisme us propery ka name hai
 
       return { ...currData }; // yaha pe hamne pure obj ko return kar diya , currData ko change kar diya and fir usi currData ko hi return kar diya
 
