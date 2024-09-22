8584ddd2aca1f37827d99bdc130e3f5a0df15486
vikas <rawatvikas689@gmail.com>
2024-09-22 11:11:41 +0530

    commit

--- form/Form.jsx
+++ form/Form.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function Form() {
   const [fullName, setFullName] = useState("");
   🌟🌟 react ne  input value ko internally control kar liya isliye use hame  CONTROLL COMPONENTS khate hai, sab state variable ke reason se hi change huwa kuki react state ko janata hai  agar hame koi value janni ho to state ki value ko deko 🌟🌟
-  let handleFullName = (e) => {
+  let handleFullName = (e) => { 
     //  console.log((e.target))
     setFullName(e.target.value);
   };
