5a6bcd97b7d5d91de4b5bccdf4b21820fd7f4a4a
vikas <rawatvikas689@gmail.com>
2024-09-22 19:34:41 +0530

    commit

--- statepractice/ObjectsAndState/Ludo.jsx
+++ statepractice/ObjectsAndState/Ludo.jsx
@@ -10,7 +10,7 @@ function Ludo() {
     setMoves({ ...moves, blue: (moves.blue += 1) }); // if we want to change the object state ,then we need to copy the state through spread operator and it will render it onclick
   };
   let updateYellow = () => {
-    moves.yellow += 1;
+    // moves.yellow += 1;
     // console.log(moves);
     // setMoves({ ...moves, yellow: (moves.yellow += 1) });
 
