51c8b9b78b615f1b04d7c3dcef772ee2f20bef71
vikas <rawatvikas689@gmail.com>
2024-09-24 19:43:20 +0530

    commit

--- statepractice/ArrayAndState/Delete.jsx
+++ statepractice/ArrayAndState/Delete.jsx
@@ -31,7 +31,7 @@
 
         {/* <div> */}
           {/* {deleteItem.map((val, indx) => ( */}
-            {/* <div key={indx}> */}
+            <div key={indx}>
               <p>{val}</p>
               <button
                 onClick={() => {
