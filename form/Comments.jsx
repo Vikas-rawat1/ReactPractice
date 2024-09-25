c4edb5a2cfcd347ffffabd1fd82036eadcec8834
vikas <rawatvikas689@gmail.com>
2024-09-25 20:24:25 +0530

    commit

--- form/Comments.jsx
+++ form/Comments.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Comments() {
   return (
     <div>
-<h1>Give a Comment</h1>
-
+      <h1>Give a Comment</h1>
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
