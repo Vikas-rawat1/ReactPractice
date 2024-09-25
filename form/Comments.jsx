bd9261bcc510155a4a62928bb37466f5eddd9138
vikas <rawatvikas689@gmail.com>
2024-09-25 20:31:33 +0530

    commit

--- form/Comments.jsx
+++ form/Comments.jsx
@@ -3,12 +3,16 @@ import React from "react";
 function Comments() {
   return (
     <div>
+    {/* Not focusing on styles here only focusing on logic */}
       <h1>Give a Comment</h1>
       <form action="">
           <input type="text" placeholder="username" />
+          <br /><br />
           <textarea>Remarks</textarea>
+          <br /><br /><br />
         <input type="number" placeholder="number" />
-        <button>Submit</button>
+        <br /><br />
+        <button>Add Commet</button>
       </form>
     </div>
   );
