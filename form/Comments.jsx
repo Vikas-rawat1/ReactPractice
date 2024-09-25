9a9fd0016e6cfac8e541687e87f0ebf97368421c
vikas <rawatvikas689@gmail.com>
2024-09-25 20:54:31 +0530

    commit

--- form/Comments.jsx
+++ form/Comments.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Comments() {
   return (
     <div>
-      {/* Not focusing on styles here only focusing on logic */}
+      Not focusing on styles here only focusing on logic
       <h1>Give a Comment</h1>
       <form action="">
         <input type="text" placeholder="username" />
