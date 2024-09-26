f8273029f0b7daf3cae976cebbb5a522fa743cc0
vikas <rawatvikas689@gmail.com>
2024-09-26 21:22:29 +0530

    commit

--- form/Comments.jsx
+++ form/Comments.jsx
@@ -1,24 +1,48 @@
 import React, { useState } from "react";
 
 function Comments() {
-  let [formData,setFormData]=useState({
-    username:'',
-    remarks:'',
-    rating:''
-  })
+  let [formData, setFormData] = useState({
+    username: "",
+    remarks: "",
+    rating: "",
+  });
+  let handleInputChange = (e) => {
+    setFormData(e.target.value);
+    // console.log(e.target.value);
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       {/* Not focusing on styles here only focusing on logic */}
       <h1>Give a Comment</h1>
-      <form action="">
-        <input type="text" placeholder="username"  value={formData.username}/>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={formData.username}
+          onChange={handleInputChange}
+        />
         <br />
         <br />
-        <textarea placeholder="Remarks" value={formData.remarks}></textarea>
+        <textarea
+          placeholder="Remarks"
+          value={formData.remarks}
+          onChange={handleInputChange}
+        ></textarea>
         <br />
         <br />
-        <br />
-        <input type="number" placeholder="Rating" style={{width:"10vw"}} min={1} max={5} value={formData.rating}/>
+        <input
+          type="number"
+          placeholder="Rating"
+          style={{ width: "10vw" }}
+          min={1}
+          max={5}
+          value={formData.rating}
+          onChange={handleInputChange}
+        />
         <br />
         <br />
         <button>Add Commet</button>
