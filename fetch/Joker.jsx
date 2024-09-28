588cc5d464b744981d29fd31a701d0b464cea4f2
vikas <rawatvikas689@gmail.com>
2024-09-28 21:06:27 +0530

    commit

--- fetch/Joker.jsx
+++ fetch/Joker.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Joker() {
+  useState();
+  const URL = "https://official-joke-api.appspot.com/random_joke";
+  const newJoke = async () => {
+    const response = await fetch(URL);
+    const jsonResponse = await response.json();
+    // console.log(URL);
+    console.log(jsonResponse.punchline);
+  };
+
   return (
-    <div>Joker</div>
-  )
+    <div>
+      <h1>Jokes</h1>
+      <h1>{joke.punchline}</h1>
+
+      <button onClick={newJoke}>New Joke</button>
+    </div>
+  );
 }
 
-export default Joker
+export default Joker;
