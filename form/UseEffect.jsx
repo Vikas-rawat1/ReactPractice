594de15dcb278fd2df9e09dcaf910d16317dd80d
vikas <rawatvikas689@gmail.com>
2024-09-28 20:34:09 +0530

    commit

--- form/UseEffect.jsx
+++ form/UseEffect.jsx
@@ -4,28 +4,30 @@ import { useEffect } from "react";
 function UseEffect() {
   const [countX, setCountX] = useState(0);
   const [countY, setCountY] = useState(0);
-  let handleClick = () => {
+  let handleClickX = () => {
     setCountX(countX + 1);
   };
-  // useEffect(function printSomething() {
-  //   console.log("This is effect");
-  // }); //jab bhi koi bhi component render hoga to ye bhi render hoga
+  useEffect(function printSomething() {
+    console.log("This is effect");
+  }); //jab bhi koi bhi component render hoga to ye bhi render hoga
 
   //passing the dependiences
-
+  let handleClickY = () => {
+    setCountX(countY + 1);
+  };
   // this are nothing but our state variables
   useEffect(
     function printSomething() {
       console.log("This is effect");
     },
     [countX]
-  ); //passing the dependiences
+  ); //passing the dependiences now the y will be not trigger
   return (
     <>
       <h1>countX={countX}</h1>
-      <button onClick={handleClick}>Count</button>
+      <button onClick={handleClickX}>Count</button>
       <h1>countY={countY}</h1>
-      <button onClick={handleClick}>Count</button>
+      <button onClick={handleClickY}>Count</button>
     </>
   );
 }
