82bbf106038bda397362f77fd1d515c6ea1e1ffe
vikas <rawatvikas689@gmail.com>
2024-09-28 20:16:36 +0530

    commit

--- form/UseEffect.jsx
+++ form/UseEffect.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 
 function UseEffect() {
   const [count, setCount] = useState(0);
-  return <>
-    <h1>count={count}</h1>
-  </>;
+  let handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h1>count={count}</h1>
+    </>
+  );
 }
 
 export default UseEffect;
