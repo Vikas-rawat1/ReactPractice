7933e382c25c659a0a5166c2effb37330d9871c5
vikas <rawatvikas689@gmail.com>
2024-09-28 20:26:38 +0530

    commit

--- form/UseEffect.jsx
+++ form/UseEffect.jsx
@@ -9,6 +9,10 @@ function UseEffect() {
   useEffect(function printSomething() {
     console.log("This is effect");
   }); //jab bhi koi bhi component render hoga to ye bhi render hoga
+
+  //passing the dependiences 
+
+  // this are nothing but our state variables 
   return (
     <>
       <h1>count={count}</h1>
