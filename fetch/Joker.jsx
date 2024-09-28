71caaa7c9d7c66d24961d91601ce15145e919487
vikas <rawatvikas689@gmail.com>
2024-09-28 22:13:51 +0530

    commit

--- fetch/Joker.jsx
+++ fetch/Joker.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 function Joker() {
-  const [joke, setJoke] = useState();
+  const [joke, setJoke] = useState(null);
   const URL = "https://official-joke-api.appspot.com/random_joke";
   const newJoke = async () => {
     const response = await fetch(URL);
     const jsonResponse = await response.json();
     // console.log(jsonResponse.setup && jsonResponse.punchline);
-    setJoke(jsonResponse);
+    console.log(jsonResponse);
     setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
   };
 
   return (
     <div>
       <h1>Jokes</h1>
-      <h1>{joke.setup}</h1>
-      <h1>{joke.punchline}</h1>
+      {joke && (
+        <>
+          <h1>{joke.setup}</h1>
+          <h1>{joke.punchline}</h1>
+        </>
+      )}
 
       <button onClick={newJoke}>New Joke</button>
     </div>
