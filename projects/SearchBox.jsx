fcfc8d711485986e7e762938d3dc05eed7acdb8b
vikas <rawatvikas689@gmail.com>
2024-10-02 22:21:52 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -1,18 +1,34 @@
 import { useState } from "react";
-
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 function SearchBox() {
-  const [inputBox, setInputBox] = useState("");
-  console.log(inputBox);
+  const [city, setCity] = useState("");
+  // console.log(city);
+
+  let handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(city)
+    setCity("")
+  };
   return (
     <>
-      <div className="bg-zinc-900 w-full h-screen">
-        <input
-          type="text"
-          placeholder="Search"
-          value={inputBox}
-          onChange={(e) => setInputBox(e.target.value)}
-        />
-        <button className="bg-red-600 w-64 h-40">Search</button>
+      <div style={{ marginLeft: "3vw", marginTop: "2vw" }}>
+        <form action="" onSubmit={handleOnSubmit}>
+          <TextField
+            id="city"
+            label="City Name"
+            variant="outlined"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <Button
+            style={{ marginTop: "0.7vw", marginLeft: "2vw" }}
+            variant="contained"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
       </div>
     </>
   );
