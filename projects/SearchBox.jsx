43462e58f8365b8e61269edf6206884878d36f31
vikas <rawatvikas689@gmail.com>
2024-10-02 21:15:18 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -1,4 +1,11 @@
 function SearchBox() {
-  return <>hello</>;
+  return (
+    <>
+      <div>
+        <input type="text" placeholder="Search" />
+        <button>Search</button>
+      </div>
+    </>
+  );
 }
 export default SearchBox;
