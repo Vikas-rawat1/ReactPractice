94b8b99f97c4f4824f4d24d9afd5020e16d70eba
vikas <rawatvikas689@gmail.com>
2024-10-02 21:16:36 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function SearchBox() {
   const [inputBox, setInputBox] = useState();
+  console.log(inputBox)
   return (
     <>
       <div>
-        <input type="text" placeholder="Search" />
+        <input type="text" placeholder="Search" value={inputBox}/>
         <button>Search</button>
       </div>
     </>
