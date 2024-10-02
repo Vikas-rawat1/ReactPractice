22106ce3d17ce30658ef34fe514b9599804184f9
vikas <rawatvikas689@gmail.com>
2024-10-02 20:50:16 +0530

    commit

--- projects/MaterialDemo.jsx
+++ projects/MaterialDemo.jsx
@@ -1,7 +1,7 @@
 // import { Button } from "@mui/material";
 // import React from "react";
 
-// function MaterialDemo() {
+function MaterialDemo() {
   return (
     <div style={{padding:"2vw"}}>
       <p> Material UI Demo</p>
