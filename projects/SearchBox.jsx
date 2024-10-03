b46e07cceeacdcf042ef014b08751008f6ad432e
vikas <rawatvikas689@gmail.com>
2024-10-03 14:07:27 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -3,7 +3,8 @@ import TextField from "@mui/material/TextField";
 import { Button } from "@mui/material";
 function SearchBox() {
   const API_URL =
-    "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    // "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    "https://api.openweathermap.org/data/2.5/weather";
   console.log(API_URL);
 
   const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
