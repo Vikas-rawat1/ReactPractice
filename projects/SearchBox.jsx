71dd5f6c290f45921e6409019cb4a9a79bb5c11c
vikas <rawatvikas689@gmail.com>
2024-10-03 14:15:22 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -12,7 +12,7 @@ function SearchBox() {
 
   let getWeatherInfo = async () => {
     let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`); //which url to get the data
-    let jsonResponse=response.json();
+    let jsonResponse= await response.json();
     console.log(jsonResponse)
   };
 
