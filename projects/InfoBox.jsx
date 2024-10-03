543d8cdf34caf2311579be54d2e20ba3256c7362
vikas <rawatvikas689@gmail.com>
2024-10-03 20:59:05 +0530

    commit

--- projects/InfoBox.jsx
+++ projects/InfoBox.jsx
@@ -1,12 +1,12 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
 
-function InfoBox({weather}) {
+function InfoBox({ weather }) {
   return (
     <div>
       {" "}
@@ -23,7 +23,7 @@ function InfoBox({weather}) {
               {}
             </Typography>
             <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            <p>Humidity {weather.humidity}</p>
+              <p>Humidity {weather.humidity}</p>
               <p>Temperature {weather.temp}</p>
               <p>Max Temperature {weather.tempMax}</p>
               <p>Min Temperature {weather.tempMin}</p>
