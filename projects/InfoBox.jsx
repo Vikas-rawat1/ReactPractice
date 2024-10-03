777e0bf1c7d945d81d04a92f77db98fc17ca92a7
vikas <rawatvikas689@gmail.com>
2024-10-03 20:48:38 +0530

    commit

--- projects/InfoBox.jsx
+++ projects/InfoBox.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function InfoBox() {
   return (
-    <div>InfoBox</div>
-  )
+    <div>
+      {" "}
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
