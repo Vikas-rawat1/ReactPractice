836889642aeee782b117db0cac71fdd281f15933
vikas <rawatvikas689@gmail.com>
2024-10-03 14:06:58 +0530

    commit

--- projects/SearchBox.jsx
+++ projects/SearchBox.jsx
@@ -6,8 +6,7 @@ function SearchBox() {
     "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
   console.log(API_URL);
 
-  const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4
-";
+  const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
 
   const [city, setCity] = useState("");
   // console.log(city);
