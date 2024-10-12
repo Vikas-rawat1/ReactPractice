291cdca7e443f98e7e298250f831acc50f492435
vikas <rawatvikas689@gmail.com>
2024-10-12 19:28:59 +0530

    commit

--- fetch/Joker.jsx
+++ fetch/Joker.jsx
@@ -1,5 +1,5 @@
 // import React, { useEffect } from "react";
-// import { useState } from "react";
+import { useState } from "react";
 //use case of useEffect 
 function Joker() {
   const [joke, setJoke] = useState(null);
