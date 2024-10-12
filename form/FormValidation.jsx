135e8e901033a9fb9aa063e66b5b1010f07d8e32
vikas <rawatvikas689@gmail.com>
2024-10-12 19:39:02 +0530

    commit

--- form/FormValidation.jsx
+++ form/FormValidation.jsx
@@ -12,26 +12,28 @@ const FormValidation = () => {
     <div style={{ marginTop: "2vw", marginLeft: "2vw" }}>
       <label htmlFor="fullName">Full Name</label>
       <br />
-      <input
-        style={{marginTop:".5vw", border: "1px solid black" }}
-        type="text"
-        placeholder="Enter text"
-        id="fullName" //🌟🌟 id dene se jaise hi ham username pe click karenege to username HIGHLIGHT ho jayega
-        value={fullName}
-        onChange={handleFullName}
-        required
-      />
-      <br />
-      <button
-        style={{
-          marginTop: "1vw",
-          background: "blue",
-          padding: ".5vw",
-          borderRadius: ".7vw",
-        }}
-      >
-        Submit
-      </button>
+      <form action="">
+        <input
+          style={{ marginTop: ".5vw", border: "1px solid black" }}
+          type="text"
+          placeholder="Enter text"
+          id="fullName" //🌟🌟 id dene se jaise hi ham username pe click karenege to username HIGHLIGHT ho jayega
+          value={fullName}
+          onChange={handleFullName}
+          required
+        />
+        <br />
+        <button
+          style={{
+            marginTop: "1vw",
+            background: "blue",
+            padding: ".5vw",
+            borderRadius: ".7vw",
+          }}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
