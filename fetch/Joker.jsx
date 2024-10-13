0d3d7ceb6be21839a36959349e5d0531f92797f4
vikas <rawatvikas689@gmail.com>
2024-10-13 12:40:17 +0530

    commit

--- fetch/Joker.jsx
+++ fetch/Joker.jsx
@@ -17,7 +17,7 @@ function Joker() {
       const response = await fetch(URL); 
       const jsonRespone = await response.json(); 
       console.log(jsonRespone); 
-      setJoke({ setup: jsonRespone.setup, punchline: jsonRespone.punchline }); 
+      setJoke({ setup: jsonRespone.setup, punchline: jsonRespone.punchline });
     }
     getNewJoke(); //useEffect me ()=>{} arrow function se hi ham define karte hai async function ko agar nahi karenge to error dega and arrow function ke baad hame use hi call karna padta hai
   }, []);
