737328004396617d2be0ec7531b4ad2b7f774757
vikas <121391039+Vikas-rawat1@users.noreply.github.com>
2024-10-21 22:11:55 +0530

    Update Cart.jsx

--- src/components/amazonCart/Cart.jsx
+++ src/components/amazonCart/Cart.jsx
@@ -4,7 +4,7 @@ function Cart() {
   const cartData = [
     {
       id: 1, 
-      title: "Logitech MX Master 3 Wireless Mouse",
+      title: "Logitech MX Master 3 Wireless Mouse", 
       description:
         "Ergonomically designed wireless mouse with customizable buttons, fast scrolling, and advanced tracking.",
       oldPrice: "₹" + 7999,
